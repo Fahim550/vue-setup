1e4ced1fb6ecc872b623842875143db1b441ee4b
Fahim550 <122682768+Fahim550@users.noreply.github.com>
2024-06-30 22:09:31 +0600

    first vue js setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+<main>
+    <h4>Fahim</h4>
+    <p>{{ clickTimes }}</p>
+    <button @click="increment">increment+</button>
+    <button @click="decrement">decrement_</button>
+</main>
+</template>
+
+<script setup>
+import {
+    ref
+} from "vue";
+const clickTimes = ref(0);
+const increment = () => {
+    clickTimes.value = clickTimes.value + 1
+    console.log(clickTimes.value);
+    console.log("hello world");
+}
+const decrement = () => {
+    if (clickTimes.value > 0) {
+
+        clickTimes.value = clickTimes.value - 1
+    }
+}
+</script>
